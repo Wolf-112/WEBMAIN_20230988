14f5efa39d52d388d2c114904520b26c1ac5f405
admin <bastad9@naver.com>
2024-05-08 13:19:36 +0900

    Main

--- js/login.js
+++ js/login.js
@@ -18,28 +18,6 @@ const check_input = () => {
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
-
-    if (emailValue.length < 5) {
-        alert('아이디는 최소 5글자 이상 입력해야 합니다.');
-        return false;
-        }
-        if (passwordValue.length < 12) {
-        alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
-        return false;
-        }
-        const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\-
-        =\[
-            \]{};':"\\|,.<>\/?]+/) !== null;
-        if (!hasSpecialChar) {
-        alert('패스워드는 특수문자를 1개 이상 포함해야 합니다.');
-        return false;
-        }
-        const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
-        const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
-        if (!hasUpperCase || !hasLowerCase) {
-        alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
-        return false;
-        }        
     };
     document.getElementById("login_btn").addEventListener('click', check_input);
     
