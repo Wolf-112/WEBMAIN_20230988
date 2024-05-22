3c19d5f4705b76546df90274f39d63abd2842e3f
admin <bastad9@naver.com>
2024-05-22 13:15:51 +0900

    Main

--- js/login.js
+++ js/login.js
@@ -15,9 +15,54 @@ const check_input = () => {
     alert('비밀번호를 입력하세요.');
     return false;
     }
+    if (emailValue.length < 5) {
+        alert('아이디는 최소 5글자 이상 입력해야 합니다.');
+        return false;
+        }
+        if (passwordValue.length < 12) {
+        alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
+        return false;
+        }
+        const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/) !== null;
+        if (!hasSpecialChar) {
+        alert('패스워드는 특수문자를 1개 이상 포함해야 합니다.');
+        return false;
+        }
+        const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
+        const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
+        if (!hasUpperCase || !hasLowerCase) {
+        alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
+        return false;
+        }  
+        const sanitizedPassword =
+    check_xss(passwordValue);
+        const sanitizedEmail = check_xss(emailValue); 
+        
+        if (!sanitizedEmail) {
+            return false;
+        }
+
+        if (!sanitizedPassword) {
+            return false;
+        }
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
     };
     document.getElementById("login_btn").addEventListener('click', check_input);
+
+    const check_xss = (input) => {
+        // DOMPurify 라이브러리 로드 (CDN 사용)
+        const DOMPurify = window.DOMPurify;
+
+        const saitizedInput = DOMPurify.sanitize(input);
+
+        if (saitizedInput !== input) {
+            alert('XSS 공격 가능성이 있는 입력값을 발견했습니다.');
+            return false;
+        }
+
+        return sanitizedInput;
+    };
+    
     
